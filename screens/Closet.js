4adb56be816efd05703b4225ff0dee900e3a6454
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-06-27 16:50:51 +0900

    base64 upload

--- screens/Closet.js
+++ screens/Closet.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import styled from "styled-components/native";
 import { FontAwesome } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
+import { Alert, Image, Dimensions } from "react-native";
+import axios from "axios";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 10px;
+  padding: 15px;
 `;
 
 const View = styled.View`
   display: flex;
-  justify-content: center;
+
   align-items: center;
 `;
 
@@ -23,35 +25,37 @@ const TouchableOpacity = styled.TouchableOpacity``;
 const Closet = () => {
   const [image, setImage] = useState(null);
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+  const pickImage = async (isTop) => {
+    console.log(isTop);
+
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
+      allowsEditing: false,
       quality: 1,
+      base64: true,
     });
 
     console.log(result);
 
     if (!result.cancelled) {
       setImage(result.uri);
+      Alert.alert("이미지 추가 완료");
     }
   };
 
   return (
     <Container>
       <View style={{ flex: 1 }}>
-        <Text>옷 추가</Text>
-        <TouchableOpacity onPress={pickImage}>
-          <FontAwesome name="plus-square" size={32} color="black" />
+        <Text>상의</Text>
+        <TouchableOpacity onPress={() => pickImage(true)}>
+          <FontAwesome name="picture-o" size={32} color="black" />
         </TouchableOpacity>
       </View>
-      <View style={{ flex: 3 }}>
-        <Text>상의</Text>
-      </View>
-      <View style={{ flex: 3 }}>
+      <View style={{ flex: 1 }}>
         <Text>하의</Text>
+        <TouchableOpacity onPress={() => pickImage(false)}>
+          <FontAwesome name="picture-o" size={32} color="black" />
+        </TouchableOpacity>
       </View>
     </Container>
   );
